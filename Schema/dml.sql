944a79ecb5ccd81314f78a2e279ca00b3fe55e0d
Brayan <baviles599@gmail.com>
2022-06-02 15:01:43 -0500

    Schema files

---
+++ Schema/dml.sql
@@ -0,0 +1,2 @@
+insert into users(username, fName, lName, birthday, city, state) values('demo', 'John', 'Doe', '2022-02-02', 'Houston', 'TX');
+insert into users(username, fName, lName, birthday, city, state) values('demo2.0', 'Jeremiah', 'Bolden', '2022-01-02', 'Philly', 'PA');

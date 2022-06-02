92abb6b0678314b1492038f73641230aa9a0a288
Hal Coronado <hallie_coronado1@baylor.edu>
2022-06-02 15:37:00 -0500

    start menu

---
+++ src/main/java/com/revature/facelessbook/ui/IMenu.java
@@ -0,0 +1,5 @@
+package com.revature.facelessbook.ui;
+
+public interface IMenu {
+    void start();
+}
---
+++ src/main/java/com/revature/facelessbook/ui/StartMenu.java
@@ -0,0 +1,51 @@
+package com.revature.facelessbook.ui;
+
+import java.util.Scanner;
+
+public class StartMenu implements IMenu{
+    @Override
+    public void start() {
+        Scanner scan = new Scanner(System.in);
+
+        exit:{
+            while(true){
+                displayWelcomeMenu();
+
+                System.out.println("\nEnter: ");
+                String input = scan.nextLine();
+
+                switch(input){
+                    case "1":
+                        login();
+                        break;
+                    case "2":
+                        signup();
+                        break;
+                    case "x":
+                        break exit;
+                    default:
+                        System.out.println("Invalid Input.");
+                        break;
+                }
+            }
+        }
+    }
+
+    private void displayWelcomeMenu(){
+        System.out.println("\nWelcome to FacelessBook!!");
+        System.out.println("[1] Login");
+        System.out.println("[2] Sign Up");
+        System.out.println("[x] Exit");
+    }
+
+
+    private void login() {
+    }
+
+    private void signup(){
+    }
+
+
+}
+
+

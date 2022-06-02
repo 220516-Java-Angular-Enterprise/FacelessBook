8afa993aec1f3fec107ac94b7dd5ce1372d26426
MrNoms <tiddle96@gmail.com>
2022-06-02 15:02:40 -0700

    Validation services

--- src/main/java/com/revature/facelessbook/Services/UserService.java
+++ src/main/java/com/revature/facelessbook/Services/UserService.java
@@ -1,21 +1,30 @@
 package com.revature.facelessbook.Services;
 
-public class UserService {
-    private Object mVar;
+import com.revature.facelessbook.daos.UserDAO;
+import com.revature.facelessbook.util.CustomException.ValidationException;
 
-    public UserService(Object obj) {
-        mVar = obj;
+public class UserService {
+    private UserDAO mUserDao;
+
+    public UserService(UserDAO uDAO) {
+        mUserDao = uDAO;
     }
 
     public boolean isValidPassword(String pass) {
-        return true;
+        if(pass.matches("^.{8,}"))
+            return true;
+        throw new ValidationException("password");
     }
 
     public boolean isValidUsername(String name) {
-        return true;
+        if(name.matches("^\\w{8,20}$"))
+            return true;
+        throw new ValidationException("username");
     }
 
     public boolean isDuplicateUsername(String name) {
-        return true;
+        if(false/*mUserDAO query for usernames and is present*/)
+            throw new ValidationException("existing username");
+        return false;
     }
 }
---
+++ src/main/java/com/revature/facelessbook/util/CustomException/ValidationException.java
@@ -0,0 +1,7 @@
+package com.revature.facelessbook.util.CustomException;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String validating) {
+        super("Failed to validate "+validating);
+    }
+}

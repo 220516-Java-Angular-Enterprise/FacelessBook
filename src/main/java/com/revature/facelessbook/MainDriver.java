15ee6a0a66b3651213328d37dc43173a1723ff3f
Hal Coronado <hallie_coronado1@baylor.edu>
2022-06-02 15:48:19 -0500

    menus

--- src/main/java/com/revature/facelessbook/MainDriver.java
+++ src/main/java/com/revature/facelessbook/MainDriver.java
@@ -1,7 +1,10 @@
 package com.revature.facelessbook;
 
+import com.revature.facelessbook.ui.StartMenu;
+
 public class MainDriver {
     public static void main(String[] args){
+        new StartMenu().start();
 
     }
 }
